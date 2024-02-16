d1c22510592cf9f4f6ca69147c4ccf8ccd98f8e9
DanielCorso009 <dcorso1@student.fitchburgstate.edu>
2024-02-15 19:15:11 -0500

    whole game is made now, enjoy!

---
+++ Assets/Scripts/ENDITALL.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ENDITALL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() => transform.Rotate(Vector3.back, 2);
+
+    void OnCollisionEnter2D(){
+        print("You WIN");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/ShooterBehavior.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    private MoveBall playerScript;
+    public GameObject player;
+    public GameObject proj;
+    private ProjBehavior projScript;
+    public float angle = 0f;
+    public Vector3 offset = new Vector3(0,0,0);
+    public int reload;
+
+    void Start()
+    {
+        playerScript = player.GetComponent<MoveBall>();
+        projScript = proj.GetComponent<ProjBehavior>();
+        reload = 120;
+        
+    }
+    void Update()
+    {
+        reload--;
+        transform.Rotate(Vector3.back, 180*Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(){
+        if(playerScript.VSpeed >playerScript.VSpeed_min&&reload<=0){
+
+            Shoot(angle,offset);
+            reload=120;
+        }
+        playerScript.rb.AddForce(new Vector2(0,100), ForceMode2D.Impulse);
+        playerScript.VSpeed =playerScript.VSpeed_min;
+    }
+    void Shoot(float facing, Vector3 offset){
+        Instantiate(proj, transform.position+offset, Quaternion.Euler(0,0,facing));
+    }
+}
